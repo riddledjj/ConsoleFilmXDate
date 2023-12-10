f765ba1be21e3e3042631621356ba9e113048175
Vadym <riddledjj@gmail.com>
2023-12-10 18:49:10 +0200

    DB connect example.

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,9 +1,8 @@
-
 CREATE SCHEMA IF NOT EXISTS public;
 
 CREATE TABLE IF NOT EXISTS movies
 (
-    id_film SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     year INTEGER
 );
@@ -11,27 +10,27 @@ CREATE TABLE IF NOT EXISTS movies
 
 CREATE TABLE IF NOT EXISTS directors
 (
-    id_director SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     fullname TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS actors
 (
-    id_actor SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     fullname TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS directors_movies
 (
 
-    id_film INTEGER REFERENCES movies,
-    id_director INTEGER REFERENCES directors,
-    PRIMARY KEY (id_film, id_director)
+    movie_id INTEGER REFERENCES movies,
+    director_id INTEGER REFERENCES directors,
+    PRIMARY KEY (movie_id, director_id)
 );
 
 CREATE TABLE IF NOT EXISTS actors_movies
 (
-    id_film INTEGER REFERENCES movies,
-    id_actor INTEGER REFERENCES actors,
-    PRIMARY KEY (id_film, id_actor)
+    movie_id INTEGER REFERENCES movies,
+    actor_id INTEGER REFERENCES actors,
+    PRIMARY KEY (movie_id, actor_id)
 )
